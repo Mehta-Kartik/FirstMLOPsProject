cbf9d6442cca6270e94910e73741715c11412d02
Mehta-Kartik <24cskar010@ldce.ac.in>
2026-03-17 19:39:40 +0530

    Data ingestion Made

---
+++ src/First_End_to_End_Project/constants/__init__.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+CONFIG_FILE_PATH = Path("D:\ProjectAarya\MLOPs\Code\Ch18 FirstMLOPs\FirstMLOPsProject\config\config.yaml")
+PARAMS_FILE_PATH = Path("D:\ProjectAarya\MLOPs\Code\Ch18 FirstMLOPs\FirstMLOPsProject\params.yaml")
+SCHEMA_FILE_PATH = Path("D:\ProjectAarya\MLOPs\Code\Ch18 FirstMLOPs\FirstMLOPsProject\schema.yaml")
+
+# print(CONFIG_FILE_PATH.exists())
+# print(PARAMS_FILE_PATH.exists())
+# print(SCHEMA_FILE_PATH.exists())
